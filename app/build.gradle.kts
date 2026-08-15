6deaaf4bade1f26184b2e342fd0304163b371d2e
balticforensics-lab <balticforensics@googlemail.com>
2026-08-15 23:52:02 +0200

    Set Java compile options to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,10 @@ android {
             )
         }
     }
-
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
     kotlinOptions {
         jvmTarget = "17"
     }
